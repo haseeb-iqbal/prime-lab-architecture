eb924cdab79b2605e1295b08fe4488d42f1143ff
haseeb-iqbal <a.haseeb19@gmail.com>
2022-02-23 05:26:01 +0000

    First Commit

---
+++ components/description/Description.styles.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+const StyledParagraph = styled.p`
+  font-size: 20px;
+  text-align: center;
+  color: red;
+`;
+export default StyledParagraph;
---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import type { NextPage } from "next";
+
+import Description from "../../components/description";
+const Dashboard: NextPage = () => {
+  return (
+    <div>
+      <Description />
+    </div>
+  );
+};
+
+export default Dashboard;
